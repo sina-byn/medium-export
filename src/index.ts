9e1a5140c93501a9f97b6549cdf58a70e3f0776b
sina-byn <sina.sina.bayan@protonmail.com>
2025-02-20 13:50:19 +0330

    feat: append command implemented

--- src/index.ts
+++ src/index.ts
@@ -27,4 +27,25 @@ program
     console.log(`Converted '${title}' to markdown`);
   });
 
+program
+  .command('append')
+  .description('Command to append Medium story markdown to a file')
+  .option('-s, --story <string>', 'Medium story url')
+  .option('-o, --output <string>', 'Output file')
+  .option('--no-author', 'Remove author info', true)
+  .option('--no-image', 'Remove images', true)
+  .action(async (opts: Options) => {
+    opts = await resolveOptions(opts);
+    const { output } = opts;
+
+    const [title, markdown] = await getMediumMarkdown(opts);
+
+    if (!fs.existsSync(output)) throw new Error(`'${output}' does not exist`);
+    if (fs.statSync(output).isDirectory()) throw new Error(`'${output}' is not a file`);
+
+    fs.appendFileSync(output, markdown, 'utf-8');
+
+    console.log(`Appended '${title}' to '${output}'`);
+  });
+
 program.parse(process.argv);
